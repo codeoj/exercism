7e376c72825dcd3f66615130b30b155ff02c8fa5
Oliver <oliverjgodoy@gmail.com>
2022-12-27 18:09:13 -0300

    solving with recursion

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,13 +1,16 @@
 defmodule BirdCount do
-  def today(list), do: Enum.at(list, 0, :nil)
+  def today([]), do: :nil
+  def today([hd | _tl]), do: hd
 
   def increment_day_count([]), do: [1]
   def increment_day_count([hd | tl]), do: [hd + 1 | tl]
-
-  def has_day_without_birds?(list), do: Enum.member?(list, 0)
+  
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _tl]), do: true
+  def has_day_without_birds?([_hd | tl]), do: has_day_without_birds?(tl)
 
   def total([]), do: 0
-  def total(list), do: Enum.reduce(list, &+/2)
+  def total([hd | tl]), do: hd + total(tl)
 
   def busy_days([]), do: 0
   def busy_days([hd | tl]) when hd >= 5, do: 1 + busy_days(tl)
